cb3b18ee1a6b9985c66abcea72a7e37790d59627
J2goode <67770226+J2goode@users.noreply.github.com>
2023-06-28 16:05:31 -0700

    Rewrote from switch/case to if/else

--- Code/Arduino Motor Control.ino
+++ Code/Arduino Motor Control.ino
@@ -4,15 +4,22 @@
 
 int t = 500; // time (microseconds)
 bool actuator = LOW;
-  
+String x;
+
+
+
 void setup()
 {
+  //Built in LED
+  pinMode(LED_BUILTIN,OUTPUT);
+
   // stepper motor pins
   pinMode(6,OUTPUT);
   pinMode(7,OUTPUT);
   
-  // linear actuator pin
-  pinMode(3,OUTPUT);
+  // linear actuator pins
+  pinMode(3,OUTPUT); // carriage arms
+  pinMode(4,OUTPUT); // stepper motor
   
   // begin serial output
   Serial.begin(9600);
@@ -20,67 +27,79 @@ void setup()
 
 void loop()
 {
-  
-  // check if someone is writing
-  while (Serial.available() == 0) {
-  }
-  
-  // read serial monitor
-  int val = Serial.parseInt();// what button is pressed
+  val = 0;
+
+  // check if something is writing, give visual confirmation if read
+  while (!Serial.available() > 0);
+  char val = Serial.read();
+
+  digitalWrite(LED_BUILTIN,HIGH);
+  delay(100);
+  digitalWrite(LED_BUILTIN,LOW);
   
   // decide if carriage goes up, down, or gets released
-  // Up = 1, Down = 2, Release/Extend = 3
+  // Raise = 1, Release Actuators = 2, Release Carriage = 3, Item Hold Actuators = 4
+  // Reset System = 5
 
   // carry out functions
-  switch (val){
-  	case 1:
-    	Serial.println("Carriage up!");
-	
+  if(val == '1'){
+    x = "Raise";
     
-    	// 'up' direction
-    	digitalWrite(6,HIGH);
-        
-    	for (int i = 0;i<200;i++){
-    		digitalWrite(7,HIGH);
-    		delayMicroseconds(t);
-    		digitalWrite(7,LOW);
-    		delayMicroseconds(t);
-        }
-    	
-    	break;
-    
-    
-    case 2:
-        Serial.println("Carriage down!");
+    // raise until you hit the switch
+    digitalWrite(6,HIGH);
+    while (i < 1000; i++){ // change the 'while' to whatever the switch pin is
+    	digitalWrite(7,HIGH);
+    	delayMicroseconds(t);
+  		digitalWrite(7,LOW);
+  		delayMicroseconds(t);
+    }
 
-    
-    	// 'down' direction
-    	digitalWrite(6,LOW);
-        
-    	for (int i = 0;i<200;i++){
-    		digitalWrite(7,HIGH);
-    		delayMicroseconds(t);
-    		digitalWrite(7,LOW);
-    		delayMicroseconds(t);
-        }
-    	
-    	break;
-    
-    case 3:
-        Serial.println("Carriage release!");
-    
-    	// retract/extend linear actuator
-    	actuator = !actuator;
-    	digitalWrite(3,actuator);
-    
-    
-    
-    
-    
-    
-    
-    
+
+    // drop a lil bit, then raise again slowly
+    digitalWrite(6,LOW);
+    for (int i = 0;i<200;i++){
+    	digitalWrite(7,HIGH);
+    	delayMicroseconds(t);
+  		digitalWrite(7,LOW);
+  		delayMicroseconds(t);
+    }
+
+    digitalWrite(6,HIGH);
+    while (i < 1000; i++){ // change the 'while' to whatever the switch pin is
+    	digitalWrite(7,HIGH);
+    	delayMicroseconds(2*t);
+  		digitalWrite(7,LOW);
+  		delayMicroseconds(2*t);
+    }    
+
+  }else if (val == '2'){
+    x = "Device Release";
+
+    digitalWrite(3,HIGH);
+
+  }else if (val == '3'){
+    x = "Carriage Release";
+
+    digitalWrite(4,HIGH);
+
+  }else if (val == '4'){
+    x = "Item Set";
+
+    // send confirmation
+
+    // check which item it is
+
+    // move actuators to the time they need
+
+  }else if (val == '5'){
+    x = "Reset";
+
+    // Open carriage arms
+    // check if recoupled (recouple if not)
+    // raise to drop height
+
+
   }
   
-  
+  Serial.println(x);
 }
