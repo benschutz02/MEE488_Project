dea524c4bf12e3a76f34ac61d110915f7c47feb4
J2goode <67770226+J2goode@users.noreply.github.com>
2023-07-03 09:34:36 -0700

    Updated pins for manual override

--- Code/Arduino Motor Control.ino
+++ Code/Arduino Motor Control.ino
@@ -10,32 +10,36 @@ const unsigned long phone = 1000;
 const unsigned long tablet = 100;
 const unsigned long laptop = 50;
 
+// pins on arduino
+int couple1 = 51;
+int couple2 = 53;
+
+int carriage1 = 41;
+int carriage2 = 43;
+
+int stop = 20;
 
 void setup()
 {
   //Built in LED
   pinMode(LED_BUILTIN,OUTPUT);
-
-  // stepper motor pins
-  pinMode(6,OUTPUT);
-  pinMode(7,OUTPUT);
   
-  // linear actuator pins
-  pinMode(40, OUTPUT);
-  pinMode(41, OUTPUT); 
-  pinMode(9, INPUT_PULLUP); // Input for Button 1
-  pinMode(10,INPUT_PULLUP); // Input for Button 2
+  // carriage actuator pins
+  pinMode(carriage1, OUTPUT);
+  pinMode(carriage2, OUTPUT); 
+  pinMode(9, INPUT_PULLUP); // Input for extend
+  pinMode(8,INPUT_PULLUP); // Input for retract
 
   // stop switch
-  pinMode(20,OUTPUT);
+  pinMode(stop,OUTPUT);
 
   // coupling/decoupling system
-  pinMode(50, OUTPUT);
-  pinMode(51, OUTPUT); 
-  pinMode(13, INPUT_PULLUP); // Input for Button 1
-  pinMode(12,INPUT_PULLUP); // Input for Button 2
-  digitalWrite(50, HIGH); // Initialize pin 50 as HIGH
-  digitalWrite(51, HIGH); // Initialize pin 51 as HIGH
+  pinMode(couple1, OUTPUT);
+  pinMode(couple2, OUTPUT); 
+  pinMode(11, INPUT_PULLUP); // Input for couple
+  pinMode(10,INPUT_PULLUP); // Input for decouple
+  digitalWrite(couple1, HIGH); // Initialize pin 50 as HIGH
+  digitalWrite(couple2, HIGH); // Initialize pin 51 as HIGH
 
   // stepper motor
   stepper.setMaxSpeed(4000);
@@ -52,7 +56,6 @@ void loop()
   char val = '0';
   int i = 0;
 
-
   /* MANUAL OVERRIDES */
 
   // stepper motor
@@ -66,11 +69,11 @@ void loop()
   }
 
   // coupling/decoupling
-  if(digitalRead(13) == HIGH & digitalRead(12) == LOW){
+  if(digitalRead(11) == HIGH & digitalRead(10) == LOW){
     // Retract linear actuator 
     digitalWrite(50, HIGH);
     digitalWrite(51, LOW);
-  }else if(digitalRead(12) == HIGH & digitalRead(13) == LOW){
+  }else if(digitalRead(10) == HIGH & digitalRead(11) == LOW){
     // Extend linear actuator 
     digitalWrite(50, LOW);
     digitalWrite(51, HIGH);
@@ -82,11 +85,11 @@ void loop()
 
   // carriage
   // coupling/decoupling
-  if(digitalRead(10) == HIGH & digitalRead(9) == LOW){
+  if(digitalRead(8) == HIGH & digitalRead(9) == LOW){
     // Retract linear actuator 
     digitalWrite(50, HIGH);
     digitalWrite(51, LOW);
-  }else if(digitalRead(9) == HIGH & digitalRead(10) == LOW){
+  }else if(digitalRead(9) == HIGH & digitalRead(8) == LOW){
     // Extend linear actuator 
     digitalWrite(50, LOW);
     digitalWrite(51, HIGH);
@@ -119,7 +122,7 @@ void loop()
     
     // raise until you hit the switch
     stepper.setSpeed(500);
-    while (i < 1000){ // change the 'while' to whatever the switch pin is
+    while (digitalRead(stop) == LOW){ // change the 'while' to whatever the switch pin is
       stepper.runSpeed();
       i++;
     }
